9f970a8ae01230f73340458450cf1573e33c7f76
Kenny Root <kenny@the-b.org>
2018-11-28 19:16:45 +0900

    Update checkstyle version

--- config/quality.gradle.kts
+++ config/quality.gradle.kts
@@ -1,11 +1,11 @@
 apply(plugin = "checkstyle")
 
 configure<CheckstyleExtension> {
-    toolVersion = "8.10"
+    toolVersion = "8.14"
     configDir = rootProject.file("config/quality/checkstyle")
 }
 
-tasks.named("check") {
+tasks.named<Task>("check") {
     dependsOn("checkstyle")
 }
 
