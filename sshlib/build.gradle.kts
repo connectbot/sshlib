cc4bd6b0d843db6a4a2b80b45cfa7fb09b02a179
Kenny Root <kenny@the-b.org>
2018-11-28 19:13:58 +0900

    Update dependency versions

--- sshlib/build.gradle.kts
+++ sshlib/build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
     `java-library`
-    id("com.jfrog.artifactory") version "4.7.5"
+    id("com.jfrog.artifactory") version "4.8.1"
     id("com.jfrog.bintray") version "1.8.4"
     id("net.researchgate.release") version "2.7.0"
+    id("com.github.ben-manes.versions") version "0.20.0"
 }
 
 apply(from = "${rootDir}/publish.gradle")
@@ -11,7 +12,7 @@ apply(from = "${rootDir}/config/quality.gradle.kts")
 dependencies {
     compile("com.jcraft:jzlib:1.1.3")
     compile("org.connectbot:simplesocks:1.0.1")
-    compile("net.i2p.crypto:eddsa:0.2.0")
+    compile("net.i2p.crypto:eddsa:0.3.0")
     compile("net.vrallev.ecc:ecc-25519-java:1.0.3")
     compile("org.connectbot.jbcrypt:jbcrypt:1.0.0")
 }
