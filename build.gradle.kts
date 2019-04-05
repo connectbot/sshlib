c78165d5ae99a0ca278138c5d304e6bfe3d5c141
Kenny Root <kenny@the-b.org>
2019-04-06 10:32:36 +1100

    Add spotless config in root project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
+import com.diffplug.spotless.extra.wtp.EclipseWtpFormatterStep
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 plugins {
     id("com.github.ben-manes.versions") version "0.21.0"
+    id("com.diffplug.gradle.spotless") version "3.21.1"
 }
 
 buildscript {
@@ -19,6 +21,45 @@ buildscript {
 allprojects {
     repositories {
         jcenter()
+    }
+}
+
+spotless {
+    java {
+        target(fileTree(".") {
+            include("**/*.java")
+            exclude("**/build", "**/out")
+        })
+        removeUnusedImports()
+        trimTrailingWhitespace()
+
+        indentWithTabs()
+        replaceRegex("class-level javadoc indentation fix", "^\\*", " *")
+        replaceRegex("method-level javadoc indentation fix", "\t\\*", "\t *")
+    }
+
+    kotlinGradle {
+        target(fileTree(".") {
+            include("**/*.gradle.kts")
+            exclude("**/build", "**/out")
+        })
+        ktlint()
+    }
+
+    format("xml") {
+        target(fileTree(".") {
+            include("config/**/*.xml", "sshlib/**/*.xml")
+            exclude("**/build", "**/out")
+        })
+
+        eclipseWtp(EclipseWtpFormatterStep.XML).configFile("spotless.xml.prefs")
+    }
+
+    format("misc") {
+        target("**/.gitignore")
+        indentWithTabs()
+        trimTrailingWhitespace()
+        endWithNewline()
     }
 }
 
