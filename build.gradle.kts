6df178c487e5d92f5fcd8d25e0794f4823cb20e3
Kenny Root <kenny@the-b.org>
2025-04-14 20:41:49 -0700

    publishing: migrate to new URL for oss releases

--- build.gradle.kts
+++ build.gradle.kts
@@ -188,6 +188,9 @@ signing {
 
 nexusPublishing {
     repositories {
-        sonatype()
+        sonatype {
+            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
+        }
     }
 }
