797e9fdd5382efb71cd3223a8684c7d8d39f7944
Kenny Root <kenny@the-b.org>
2025-04-15 22:37:39 -0700

    EdDSA: update handling to allow other providers
    Allow keys from other providers by implementing engineTranslateKey. This
    will hopefully allow us to handle the new Android implementation without
    inspecting it deeply.

--- src/test/java/com/trilead/ssh2/crypto/keys/Ed25519KeyFactoryTest.java
+++ src/test/java/com/trilead/ssh2/crypto/keys/Ed25519KeyFactoryTest.java
@@ -3,11 +3,17 @@ package com.trilead.ssh2.crypto.keys;
 import org.junit.Test;
 
 import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertArrayEquals;
 
 public class Ed25519KeyFactoryTest {
 	private static final byte[] PRIVATE = toByteArray("302e020100300506032b657004220420f72a0a036e3479e15edb74da5f2a5418e66db450ad50687cad90247eeab6440c");
@@ -40,5 +46,39 @@ public class Ed25519KeyFactoryTest {
 		KeyFactory kf = KeyFactory.getInstance("Ed25519", p);
 		Ed25519PublicKey pub = (Ed25519PublicKey) kf.generatePublic(new X509EncodedKeySpec(PUBLIC));
 		assertThat(pub.getAbyte(), is(KAT_ED25519_PUB));
+	}
+
+	@Test
+	public void translatesNativeJDKKeys() throws Exception {
+		KeyPairGenerator kpg;
+		try {
+			kpg = KeyPairGenerator.getInstance("EdDSA");
+		} catch (NoSuchAlgorithmException e) {
+			// Skip test if EdDSA is not supported by the JDK
+			System.err.println("Skipping translatesNativeJDKKeys test: EdDSA not supported by this JDK");
+			return;
+		}
+
+		KeyPair keyPair = kpg.generateKeyPair();
+		PublicKey nativePubKey = keyPair.getPublic();
+		PrivateKey nativePrivKey = keyPair.getPrivate();
+
+		Ed25519KeyFactory keyFactorySpi = new Ed25519KeyFactory();
+
+		// Translate Public Key
+		PublicKey translatedPubKey = (PublicKey) keyFactorySpi.engineTranslateKey(nativePubKey);
+		assertThat(translatedPubKey.getAlgorithm(), is("EdDSA"));
+		assertThat(translatedPubKey.getFormat(), is("X.509"));
+		assertArrayEquals(nativePubKey.getEncoded(), translatedPubKey.getEncoded());
+		assertThat(((Ed25519PublicKey) translatedPubKey).getAbyte(), is(((Ed25519PublicKey) keyFactorySpi
+				.engineGeneratePublic(new X509EncodedKeySpec(nativePubKey.getEncoded()))).getAbyte()));
+
+		// Translate Private Key
+		PrivateKey translatedPrivKey = (PrivateKey) keyFactorySpi.engineTranslateKey(nativePrivKey);
+		assertThat(translatedPrivKey.getAlgorithm(), is("EdDSA"));
+		assertThat(translatedPrivKey.getFormat(), is("PKCS#8"));
+		assertArrayEquals(nativePrivKey.getEncoded(), translatedPrivKey.getEncoded());
+		assertThat(((Ed25519PrivateKey) translatedPrivKey).getSeed(), is(((Ed25519PrivateKey) keyFactorySpi
+				.engineGeneratePrivate(new PKCS8EncodedKeySpec(nativePrivKey.getEncoded()))).getSeed()));
 	}
 }
