af4d2679b9d10e9b5dd1bd8b2ac74416ab0d66a1
Kenny Root <kenny@the-b.org>
2026-02-01 18:57:28 -0800

    feat(cipher): implement 3des-cbc as cipher
    Add 3des-cbc using DESede/CBC/NoPadding and add it to the end of
    requested cipher algorithms. Add unit tests and add to integration
    tests.

---
+++ sshlib/src/main/kotlin/org/connectbot/sshlib/crypto/TripleDesCbcCipher.kt
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2025 Kenny Root
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.connectbot.sshlib.crypto
+
+import javax.crypto.Cipher
+import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.SecretKeySpec
+
+/**
+ * 3DES-CBC cipher implementation for SSH packet encryption/decryption.
+ *
+ * As specified in RFC 4253.
+ *
+ * Note: Each instance should be used for only one direction (encrypt OR decrypt).
+ * CBC mode maintains internal state (IV) across multiple packets.
+ *
+ * @param key Encryption key (24 bytes)
+ * @param iv Initial IV (8 bytes)
+ * @param forEncryption True for encryption, false for decryption
+ */
+class TripleDesCbcCipher(
+    private val key: ByteArray,
+    private val iv: ByteArray,
+    forEncryption: Boolean
+) : PacketCipher {
+    override val blockSize: Int = 8
+
+    private val cipher: Cipher = Cipher.getInstance("DESede/CBC/NoPadding")
+
+    init {
+        require(key.size == 24) {
+            "3DES key must be 24 bytes, got ${key.size}"
+        }
+        require(iv.size == 8) {
+            "IV must be 8 bytes, got ${iv.size}"
+        }
+
+        val keySpec = SecretKeySpec(key, "DESede")
+        val ivSpec = IvParameterSpec(iv)
+        val mode = if (forEncryption) Cipher.ENCRYPT_MODE else Cipher.DECRYPT_MODE
+        cipher.init(mode, keySpec, ivSpec)
+    }
+
+    override fun encrypt(data: ByteArray): ByteArray {
+        return cipher.update(data)
+    }
+
+    override fun decrypt(data: ByteArray): ByteArray {
+        return cipher.update(data)
+    }
+}
